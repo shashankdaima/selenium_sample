ba3664accaddefbc9e32fc6f1f1767f857780cf9
shashank <daimashashank10@gmail.com>
2023-03-15 06:00:22 +0530

    chrome driver added.

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+# from selenium import webdriver
+# #from selenium.webdriver.chrome.options import Options
+# chromeOptions = webdriver.ChromeOptions() 
+# chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) 
+# chromeOptions.add_argument("--no-sandbox") 
+# chromeOptions.add_argument("--disable-setuid-sandbox") 
+# chromeOptions.add_argument("--headless")
+# chromeOptions.add_argument("--remote-debugging-port=9222")  # this
+
+# chromeOptions.add_argument("--disable-dev-shm-using") 
+# chromeOptions.add_argument("--disable-extensions") 
+# chromeOptions.add_argument("--disable-gpu") 
+# chromeOptions.add_argument("start-maximized") 
+# chromeOptions.add_argument("disable-infobars")
+# chromeOptions.add_argument(r"user-data-dir=./cookies/test") 
+
+# b = webdriver.Chrome(chrome_options=chromeOptions) 
+# b.get("https://www.geeksforgeeks.org/how-to-get-title-of-a-webpage-using-selenium-in-python/") 
+# print(b.title)
+# b.quit()
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
-chromeOptions = webdriver.ChromeOptions() 
+from selenium.webdriver.chrome.options import Options
+
+chromeOptions = Options()
+# options.headless = True
 chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) 
 chromeOptions.add_argument("--no-sandbox") 
 chromeOptions.add_argument("--disable-setuid-sandbox") 
@@ -12,9 +34,7 @@ chromeOptions.add_argument("--disable-extensions")
 chromeOptions.add_argument("--disable-gpu") 
 chromeOptions.add_argument("start-maximized") 
 chromeOptions.add_argument("disable-infobars")
-chromeOptions.add_argument(r"user-data-dir=.\cookies\\test") 
-
-b = webdriver.Chrome(chrome_options=chromeOptions) 
-b.get("https://www.geeksforgeeks.org/how-to-get-title-of-a-webpage-using-selenium-in-python/") 
-print(b.title)
-b.quit()
+chromeOptions.add_argument(r"user-data-dir=./cookies/test") 
+driver = webdriver.Chrome("./chromedriver", chrome_options=chromeOptions)
+driver.get("https://www.geeksforgeeks.org/how-to-get-title-of-a-webpage-using-selenium-in-python/")
+print(driver.title)
