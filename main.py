4bbc263fb3a049fdc827be56cf5b2d84a1d89803
Shashank Daima <64317542+shashankdaima@users.noreply.github.com>
2023-03-18 03:24:28 +0530

    Removed Comments

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-# from selenium import webdriver
-# #from selenium.webdriver.chrome.options import Options
-# chromeOptions = webdriver.ChromeOptions() 
-# chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) 
-# chromeOptions.add_argument("--no-sandbox") 
-# chromeOptions.add_argument("--disable-setuid-sandbox") 
-# chromeOptions.add_argument("--headless")
-# chromeOptions.add_argument("--remote-debugging-port=9222")  # this
-
-# chromeOptions.add_argument("--disable-dev-shm-using") 
-# chromeOptions.add_argument("--disable-extensions") 
-# chromeOptions.add_argument("--disable-gpu") 
-# chromeOptions.add_argument("start-maximized") 
-# chromeOptions.add_argument("disable-infobars")
-# chromeOptions.add_argument(r"user-data-dir=./cookies/test") 
-
-# b = webdriver.Chrome(chrome_options=chromeOptions) 
-# b.get("https://www.geeksforgeeks.org/how-to-get-title-of-a-webpage-using-selenium-in-python/") 
-# print(b.title)
-# b.quit()
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
